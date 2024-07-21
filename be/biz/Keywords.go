887c5e9814c56d301875322d753c4f4c538b6fbd
江键翔 <1364054111@qq.com>
2024-07-21 20:27:45 +0800

    refactor:使用gen框架代替底层手写curd

---
+++ be/biz/Keywords.go
@@ -0,0 +1,34 @@
+package biz
+
+import (
+	"github.com/Milefer7/compliation_exp/dal/model"
+	"github.com/Milefer7/compliation_exp/dal/query"
+	"github.com/Milefer7/compliation_exp/dao/mysql"
+	"github.com/gin-gonic/gin"
+)
+
+func ReadKeywords(c *gin.Context) ([]*model.Keywords, error) {
+	var q = query.Use(mysql.Db)
+	Q := q.Keywords.WithContext(c.Request.Context())
+	keywords, err := Q.Find()
+	if err != nil {
+		return nil, err
+	}
+	return keywords, nil
+}
+
+func UpdateKeywords(u []*model.Keywords) error {
+	err := query.Q.Keywords.Save(u...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateKeywords(keywords []*model.Keywords) error {
+	err := query.Q.Keywords.Create(keywords...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
